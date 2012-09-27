dc8fe9066b0e8dae8801fd2ffbec12198cb55896
unknown <tafeng.dxx@JHGH-RMPPZY3OCD.taobao.ali.com>
2012-09-27 21:04:58 +0800

    add sendMsg function

--- background.js
+++ background.js
@@ -1,6 +1,27 @@
-var DN = window.XX.DesktopNotify;
-DN.show('./icon.png',  'Hello!', 'Lorem ipsum...');
+void function() {
+	var TITLE = '尿歇时间到啦~~！亲！！', 
+		ICONURL = 'icon.png', 
+		DN = window.XX.DesktopNotify,
+		handler = null;
+	
+	function showDialog(icon, title, value, duration) {
+		console.log(new Date());
+		DN.show(icon, title, value);
+		console.log(icon, title, value)
+		setTimeout(function() {
+			DN.hide();
+		}, duration);
+	}
 
-chrome.extension.onMessage.addListener(function(detail){
-	console.log(detail);
-});
+	chrome.extension.onMessage.addListener(function(msg) {
+		if(msg.stop) {
+			handler && clearInterval(handler);
+			return;
+		}
+		
+		DN.requestPermission(function(){
+			handler = setInterval(showDialog, msg.time, ICONURL, TITLE, msg.value, msg.duration);
+		});
+	});
+}();
+
--- desktop-notification.js
+++ desktop-notification.js
@@ -1,38 +1,32 @@
 void function() {
-	var TITLE = '时间到啦~~！亲！！',
-		ICONURL = 'icon.png';
-	
 	var DN = window.XX.DesktopNotify;
-	
-	function notify(icon, title, value, duration) {
-		DN.show(icon, title, value);
-		setTimeout(function() {
-			DN.hide();
-		}, duration);
+
+	function createMsg() {
+		return {
+			time : timeEl.value * 60 * 1000,
+			duration : durEl.value * 60 * 1000,
+			value : msgEl.value
+		}
 	}
-	
-	if(!DN.isSupport()) {
+
+	if (!DN.isSupport()) {
 		alert('浏览器不支持桌面通知！');
 		return;
 	}
-	
-	var startEl = document.getElementById('start'),
-		stopEl = document.getElementById('stop'),
-		timeEl = document.getElementById('time'),
-		msgEl = document.getElementById('msg'),
-		durEl = document.getElementById('duration'),
-		handler = null;
-	
+
+	var startEl = document.getElementById('start'), 
+		stopEl = document.getElementById('stop'), 
+		timeEl = document.getElementById('time'), 
+		msgEl = document.getElementById('msg'), 
+		durEl = document.getElementById('duration');
+
 	startEl.addEventListener('click', function(evt) {
-		DN.requestPermission(function() {
-			handler = setInterval(notify, timeEl.value * 60 * 1000, ICONURL, TITLE, msgEl.value, durEl.value*60*1000);
-			startEl.disabled = true;
-			chrome.extension.sendMessage({data:"hello world!"});
-		});
+		startEl.disabled = true;
+		chrome.extension.sendMessage(createMsg());
 	}, false);
-	
+
 	stopEl.addEventListener('click', function(evt) {
-		clearInterval(handler);
+		chrome.extension.sendMessage({stop:1});
 		startEl.disabled = false;
 	}, false);
 }();
