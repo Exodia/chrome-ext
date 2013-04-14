9c43ebc0e66878f16007c3503693b1a5a1d6d6a3
Exodia <d_xinxin@163.com>
2013-04-15 01:37:22 +0800

    直接在页面加载时插入脚本

--- tow/main.js
+++ tow/main.js
@@ -1,30 +1,37 @@
-chrome.extension.onMessage.addListener(function (command) {
-    if (command == 'tow_start') {
-        startTow()
-    }
-})
+~function () {
+    var towLoaded = false;
 
-function startTow() {
-    var s = document.getElementById('J_TowForChrome')
-    if (!s) {
-        s = document.createElement('script')
-        s.id = "J_TowForChrome"
-        s.src = "https://towtruck.mozillalabs.com/towtruck.js"
-        s.onload = createExecScript;
-
-        document.head.appendChild(s)
-    } else {
-        createExecScript()
-    }
-
-}
-
-function createExecScript() {
     var s = document.createElement('script')
+    s.id = "J_TowForChrome"
+    s.src = "https://towtruck.mozillalabs.com/towtruck.js"
     s.onload = function () {
-        this.parentNode.removeChild(this)
-    }
-    s.text = 'TowTruck()'
+        towLoaded = true;
+    };
     document.head.appendChild(s)
 
-}
+    chrome.extension.onMessage.addListener(function (command) {
+        command == 'tow_start' && createExecScript()
+
+    })
+
+    function createExecScript() {
+
+        var exec = document.createElement('script')
+        exec.onload = function () {
+            this.parentNode.removeChild(this)
+        }
+        exec.text = 'TowTruck()'
+        if (towLoaded) {
+            document.head.appendChild(exec)
+
+        } else {
+            s.onload = function () {
+                towLoaded = true;
+                document.head.appendChild(exec)
+            }
+
+        }
+
+    }
+
+}()
